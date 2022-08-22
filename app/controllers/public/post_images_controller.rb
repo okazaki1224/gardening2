2f0f4cd8758828a58d724f90a865cb6121267af0
EC2 Default User <guangcheng1224@gmail.com>
2022-08-22 16:20:36 +0000

    [Add]会員側のｺﾝﾄﾛｰﾗとﾋﾞｭｰをとりあえず作成

---
+++ app/controllers/public/post_images_controller.rb
@@ -0,0 +1,10 @@
+class Public::PostImagesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+    #deviseで作ったものは最終的に一番上に！
   devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
@@ -7,5 +8,37 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
   sessions: "admin/sessions"
 }
+
+
+
+  scope module: :public do
+    root to: 'homes#top'
+    #get 'homes/top'
+    #get 'homes/about'
+    get '/about' => 'homes#about', as: "about"
+
+    resources:favorites, only:[:create, :destroy]
+    #get 'favorites/create'
+    #get 'favorites/destroy'
+    resources:comments, only:[:create, :destroy]
+    #get 'comments/create'
+    #get 'comments/destroy'
+    resources:users, only:[:index, :show, :edit, :update]
+    #get 'users/index'
+    #get 'users/show'
+    #get 'users/edit'
+    #get 'users/update'
+    resources:post_images, only:[:index, :show, :new, :create, :destroy]
+    #get 'post_images/index'
+    #get 'post_images/show'
+    #get 'post_images/new'
+    resources:posts, only:[:index, :show, :new, :create, :destroy]
+    #get 'posts/index'
+    #get 'posts/show'
+    #get 'posts/new'
+
+  end
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
