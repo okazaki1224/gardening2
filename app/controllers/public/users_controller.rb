e0f8064f8def9271a1502df508b7180a1c6ce010
EC2 Default User <guangcheng1224@gmail.com>
2022-08-26 15:16:04 +0000

    [Update]会員一覧/詳細

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -11,8 +11,22 @@ class Public::UsersController < ApplicationController
   end
 
   def edit
+    @user=User.find(params[:id])
   end
 
   def update
+    @user=User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user.id),notice: "プロフィールを更新しました！"
+    else
+      render :edit
+    end
   end
+
+  private
+  def user_params
+      params.require(:user) .permit(:name, :introduction, :profile_image)
+  end
+
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_24_101945) do
+ActiveRecord::Schema.define(version: 2022_08_26_141833) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -80,6 +80,7 @@ ActiveRecord::Schema.define(version: 2022_08_24_101945) do
     t.string "last_name", null: false
     t.string "first_name", null: false
     t.string "nickname", null: false
+    t.string "introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
