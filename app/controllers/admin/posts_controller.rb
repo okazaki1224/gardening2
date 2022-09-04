a2964346f71c9be200344d78abc292bf0efcd1aa
EC2 Default User <guangcheng1224@gmail.com>
2022-09-04 04:59:55 +0000

    [Add]管理者側に投稿削除ボタンを設置

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -13,12 +13,17 @@ class Admin::PostsController < ApplicationController
 
   def index
     @posts = Post.all
-
   end
 
   def show
     @post=Post.find(params[:id])
-    
+  end
+
+  def destroy
+    @posts=Post.all
+    @post=Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_root_path
   end
 
   private
