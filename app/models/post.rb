d868b1851f48e6a4833eba959ff11b6f4cf9b115
EC2 Default User <guangcheng1224@gmail.com>
2022-09-13 16:43:56 +0000

    [Add]新規投稿画面にエラーメッセージを実装

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,6 +8,8 @@ class Post < ApplicationRecord
   #ジャンルを複数設定したい場合は中間テーブルを作る
   has_one_attached :image
   has_many_attached :post_images
+  
+  validates:title, presence:true#タイトルは必須
 
   def favorited_by?(user)
     favorites.exists?(user_id: user.id)
