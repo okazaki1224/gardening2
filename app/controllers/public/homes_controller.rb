b3fa9fa67a1b62363c3487c99cd2e985df30f0f0
EC2 Default User <guangcheng1224@gmail.com>
2022-09-14 15:48:36 +0000

    [Update]グリッドシステムで３：９に

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   def top
     @posts = Post.order('id DESC').limit(3)
+    @tag_lists=Tag.all
   end
 
   def about
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -2,10 +2,10 @@
 
 class Public::RegistrationsController < Devise::RegistrationsController
   before_action :check_guest, only: %i[update destroy]
-  
+
   def check_guest
     if resource.email == 'guest@example.com'
-     redirect_to root_path, alert: 'ゲストユーザーの変更・削除できません。'
+     redirect_to root_path, alert: 'ゲストユーザーの変更・削除はできません。'
     end
   end
   # before_action :configure_sign_up_params, only: [:create]
