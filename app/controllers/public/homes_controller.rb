7b0bb3a6ea3ec024223175d7b1f0f421cc73bbfe
EC2 Default User <guangcheng1224@gmail.com>
2022-09-15 14:29:40 +0000

    [Update]

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,6 @@
 class Public::HomesController < ApplicationController
   def top
-    @posts = Post.order('id DESC').limit(3)
+    @posts = Post.order('id DESC').limit(7)
     @tag_lists=Tag.all
   end
 
