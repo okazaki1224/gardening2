b37268a6276f24ccb2f2a08745492d49474e8a8f
EC2 Default User <guangcheng1224@gmail.com>
2022-10-22 12:41:05 +0000

    [Fix]管理者側のコメント管理機能の記述を修正

--- app/controllers/admin/post_comments_controller.rb
+++ app/controllers/admin/post_comments_controller.rb
@@ -9,7 +9,7 @@ class Admin::PostCommentsController < ApplicationController
   end
 
   def update
-    @post=Post.find(params[:id])
+    @post=Post.find(params[:post_id])#ここはpost_commentｺﾝﾄﾛｰﾗなのでparams[:id]だとコメントのIDを取得してしまう。当然15行目にも影響が出る（出ていた
     @post_comment=PostComment.find(params[:id])
     @post_comment.update(post_comment_params)
     redirect_to admin_post_path(@post.id)
