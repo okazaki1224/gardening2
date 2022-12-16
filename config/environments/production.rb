89971ea5e7108cbaa6860819ffc41972e9123186
EC2 Default User <guangcheng1224@gmail.com>
2022-12-16 15:26:57 +0000

    [Update]environments/developmentとproduction修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,6 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'oniwanotomo.com', port: 3000}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       port: 587,
