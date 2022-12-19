a27d4c9d22caae065dec114fea0855aa2bfd50ef
EC2 Default User <guangcheng1224@gmail.com>
2022-12-19 16:16:00 +0000

    [Update]メール機能全体を微修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,7 +118,7 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
-  config.action_mailer.default_url_options = { host: 'http://oniwanotomo.com'}
+  config.action_mailer.default_url_options = { protocol: 'http', host: 'oniwanotomo.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
       port: 587,
