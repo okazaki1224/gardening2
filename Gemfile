ef397d649d4ab5d8b550e5ba98415077b77bb682
EC2 Default User <ec2-user@ip-172-31-92-112.ec2.internal>
2022-12-28 07:56:48 +0000

    Revert "[Test]AWS SES導入試験"
    This reverts commit 3294cda96798ca17fa43ee380405f6b5c94180d6.

--- Gemfile
+++ Gemfile
@@ -69,5 +69,3 @@ end
 gem "net-smtp"
 gem "net-pop"
 gem "net-imap"
-
-gem 'aws-sdk-rails'
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   default from: '自分'
-  default to: ENV["TOMAIL"]
+  default to:"gcokzk1224@gmail.com"#元々ENV["TOMAIL"]だった
   layout 'mailer'
 end
